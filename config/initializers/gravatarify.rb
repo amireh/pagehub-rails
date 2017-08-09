39279343f926f52c7e85a29201dc5ed25412b9aa
Ahmad Amireh <ahmad@amireh.net>
2017-08-09 12:09:17 +0300

    using secure gravatars

--- config/initializers/gravatarify.rb
+++ config/initializers/gravatarify.rb
@@ -1,4 +1,5 @@
 Gravatarify.options[:filetype] = :png
+Gravatarify.options[:secure] = true
 Gravatarify.styles[:mini] = { size: 16, html: { :class => 'gravatar gravatar-mini' } }
 Gravatarify.styles[:icon] = { size: 32, html: { :class => 'gravatar gravatar-icon' } }
 Gravatarify.styles[:default] = { size: 96, html: { :class => 'gravatar' } }
